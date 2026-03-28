6eb8f8c4a91ce1f38951dfc55b0f29a2bf6bdece
jose maria <jose.maria.montenegro@gmail.com>
2026-03-28 01:24:34 -0300

    fotos

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
     // 1. Reducir la calidad global (por defecto es 75)
     // Un valor entre 60 y 70 es el "sweet spot" para móviles sin perder mucha nitidez.
     deviceSizes: [640, 750, 828, 1080, 1200], 
@@ -14,6 +22,6 @@ const nextConfig = {
     // 31536000 es un año. Esto evita que el iPhone re-descargue la imagen constantemente.
     minimumCacheTTL: 60,
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
